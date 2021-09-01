1e01b18ce52f9f5690e12415b0d3e6e4428d5320
Bruno Matheus Ferrari <83819089+Brunomatheusferrari@users.noreply.github.com>
2021-08-31 22:37:55 -0300

    Delete aula63 directory

--- aula63/config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    logging: false
-};
